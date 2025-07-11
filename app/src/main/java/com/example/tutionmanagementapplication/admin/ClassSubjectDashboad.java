3022e08f24dc2a63b8a8931ec6d0f2a45481bc3a
Chamath Nimnada <nimnadachamath25@gmail.com>
2025-07-12 02:25:33 +0530

    Upgrades

---
+++ app/src/main/java/com/example/tutionmanagementapplication/admin/ClassSubjectDashboad.java
@@ -0,0 +1,47 @@
+package com.example.tutionmanagementapplication.admin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tutionmanagementapplication.R;
+
+public class ClassSubjectDashboad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_class_subject_dashboad);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Navigations
+        LinearLayout course = findViewById(R.id.navDashboard);
+        course.setOnClickListener(view -> {
+            Intent intent = new Intent(this, AdminDashboard.class);
+            startActivity(intent);
+        });
+
+        LinearLayout dashboard = findViewById(R.id.navClassSub);
+        dashboard.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ClassSubjectDashboad.class);
+            startActivity(intent);
+        });
+
+        LinearLayout announcements = findViewById(R.id.navProfile);
+        announcements.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ProfileDashboard.class);
+            startActivity(intent);
+        });
+    }
+}
---
+++ app/src/main/java/com/example/tutionmanagementapplication/admin/ProfileDashboard.java
@@ -0,0 +1,47 @@
+package com.example.tutionmanagementapplication.admin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tutionmanagementapplication.R;
+
+public class ProfileDashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile_dashboard2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Navigations
+        LinearLayout dashboard = findViewById(R.id.navDashboard);
+        dashboard.setOnClickListener(view -> {
+            Intent intent = new Intent(this, AdminDashboard.class);
+            startActivity(intent);
+        });
+
+        LinearLayout classSub = findViewById(R.id.navSub);
+        classSub.setOnClickListener(view -> {
+            Intent intent = new Intent(this, ClassSubjectDashboad.class);
+            startActivity(intent);
+        });
+
+        LinearLayout announcements = findViewById(R.id.navUser);
+        announcements.setOnClickListener(view -> {
+            Intent intent = new Intent(this, UsersDashboard.class);
+            startActivity(intent);
+        });
+    }
+}
